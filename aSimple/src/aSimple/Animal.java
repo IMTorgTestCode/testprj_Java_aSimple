6609d88dad1ed900b3bec6e095661d8601d2b42e
IMTorg <jason.beach@mgmt-tech.org>
2015-12-10 15:33:23 +0000

    new class Animal with many new fields

---
+++ aSimple/src/aSimple/Animal.java
@@ -0,0 +1,15 @@
+package aSimple;
+
+public class Animal {
+	
+	public static final int MaxAnimals = 10;
+	
+	protected static int NumOfAnimals;
+	
+	private String sound = "grrr";
+	private double weight = 0;
+	private String color = "gray";
+	
+	
+
+}
